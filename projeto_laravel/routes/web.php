60713c65f1800d2768de72e5030a0f549fdffad9
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-03-30 09:54:57 -0300

    feat(forum-rotes): rotas index e show de fóruns adicionadas

--- projeto_laravel/routes/web.php
+++ projeto_laravel/routes/web.php
@@ -2,14 +2,21 @@
 
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\ForumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// users routes
 Route::get('/usuarios', [UsuarioController::class, 'index']);
 Route::get('/usuario/{id}', [UsuarioController::class, 'show']);
 
+// products routes
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::get('/produto/{id}', [ProdutoController::class, 'show']);
+
+// forums routes
+Route::get('/forums', [ForumController::class, 'index']);
+Route::get('/forum/{id}', [ForumController::class, 'show']);
