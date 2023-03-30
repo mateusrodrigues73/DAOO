33a3b10b89ee82897a3ff5563f5f6a07ffe60d39
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-03-30 08:58:10 -0300

    feat(user-show-view): função show adicionada as views de usuarios

---
+++ projeto_laravel/resources/views/usuarios/show.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuário</title>
+</head>
+<body>
+    <h1>Usuário</h1>
+    @if (isset($usuario))
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Sobrenome</th>
+                <th>Email</th>
+                <th>Senha</th>
+                <th>Estatus</th>
+                <th>Imagem</th>
+                <th>Administrador</th>
+                <th>Media de avaliações</th>
+                <th>Total de avaliações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{$usuario->id}}</td>
+                <td>{{$usuario->nome}}</td>
+                <td>{{$usuario->sobrenome}}</td>
+                <td>{{$usuario->email}}</td>
+                <td>{{$usuario->senha}}</td>
+                <td>{{($usuario->estatus)?'Verificado':'Não verificado'}}</td>
+                <td>{{$usuario->imagem}}</td>
+                <td>{{($usuario->administrador)?'Sim':'Não'}}</td>
+                <td>{{$usuario->media_de_avaaliacoes}}</td>
+                <td>{{$usuario->total_de_avaliacoes}}</td>
+            </tr>
+        </tbody>
+    </table>
+    @else
+    <p>Usuário não encontrado! </p>
+    @endif
+    <a href="/usuarios/">
+        Voltar
+    </a>
+</body>
+</html>
