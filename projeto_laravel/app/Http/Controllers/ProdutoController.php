330c966398fbfd62bf3a62eec698d6c010d86b24
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-03-30 09:56:35 -0300

    feat(product-controller): controller da classe Produto  adicionado

--- projeto_laravel/app/Http/Controllers/ProdutoController.php
+++ projeto_laravel/app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
-    //
+    private $produto;
+
+    public function __construct()
+    {
+        $this->produto = new Produto();
+    }
+
+    public function index()
+    {
+        return view('produtos.index', ['produtos'=>$this->produto->all()]);
+    }
+
+    public function show($id)
+    {
+        return view('produtos.show', ['produto'=>$this->produto->find($id)]);
+    }
 }
