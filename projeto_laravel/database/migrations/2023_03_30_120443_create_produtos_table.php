00d26254ac112432303387d0fb932962ad043bc3
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-03-30 09:33:08 -0300

    feat(user-migration): migration para criação da tabela produtos adicionada

---
+++ projeto_laravel/database/migrations/2023_03_30_120443_create_produtos_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->text('modelo');
+            $table->text('marca');
+            $table->text('categoria');
+            $table->text('informacoes');
+            $table->float('preco');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
