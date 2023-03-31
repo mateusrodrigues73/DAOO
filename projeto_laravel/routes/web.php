19ced0f207aa38f6d4d015a092eccdf78bc4c6a2
Aluno <aluno@example.com>
2023-03-31 17:41:06 -0300

    feat(user-route): rotas para criação edição e exclusão de usuários adicionadas

--- projeto_laravel/routes/web.php
+++ projeto_laravel/routes/web.php
@@ -12,6 +12,12 @@ Route::get('/', function () {
 // users routes
 Route::get('/usuarios', [UsuarioController::class, 'index']);
 Route::get('/usuario/{id}', [UsuarioController::class, 'show']);
+Route::get('usuario', [UsuarioController::class,'create']);
+Route::post('usuario', [UsuarioController::class,'store']);
+Route::get('usuario/{id}/edit', [UsuarioController::class, 'edit'])->name('usuario.edit');
+Route::post('usuario/{id}/update', [UsuarioController::class,'update'])->name('usuario.update');
+Route::get('usuario/{id}/delete', [UsuarioController::class,'delete'])->name('usuario.delete');
+Route::post('usuario/{id}/remove', [UsuarioController::class,'destroy'])->name('usuario.remove');
 
 // products routes
 Route::get('/produtos', [ProdutoController::class, 'index']);
