7f199bee6597e7e4dc60c543c1ac9cf6c09b6c99
Aluno <aluno@example.com>
2023-03-31 17:44:27 -0300

    feat(user-controller-functions): adicionadas as funções para adição, atualização e exclusão de usuários

--- projeto_laravel/app/Http/Controllers/UsuarioController.php
+++ projeto_laravel/app/Http/Controllers/UsuarioController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 use App\Models\Usuario;
 
 class UsuarioController extends Controller
@@ -22,4 +24,51 @@ class UsuarioController extends Controller
     {
         return view('usuarios.show', ['usuario'=>$this->usuario->find($id)]);
     }
+
+    public function create()
+    {
+        return view('usuarios.create');
+    }
+
+    public function store(Request $request)
+    {
+        $newUsuario = $request->all();
+        if (!Usuario::create($newUsuario)) {
+            dd("Error ao criar usuário!!");
+        }
+        return redirect('/usuarios');
+    }
+
+    public function edit($id)
+    {
+        return view('usuarios.edit',[
+            'usuario'=>Usuario::find($id)
+        ]);
+    }
+
+    public function update(Request $request,$id)
+    {
+        $newUsuario = $request->all();
+        if (!Usuario::find($id)->update($newUsuario)) {
+            dd("Error ao criar usuário!!");
+        }
+        return redirect('/usuarios');
+    }
+
+    public function delete($id)
+    {
+        return view('usuarios.delete',[
+            'usuario'=>Usuario::find($id)
+        ]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if($request->has('confirmar'))
+            if (!Usuario::destroy($id))
+                dd("Error ao deletar usuário!!");
+
+        return redirect('/usuarios');
+    }
+
 }
