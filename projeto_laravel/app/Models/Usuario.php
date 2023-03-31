dcfcf0553972a939f8242d92136ac341a1cf5ca9
Aluno <aluno@example.com>
2023-03-31 17:47:34 -0300

    feat(user-model-fillable): adição do array com os campos a serem preenchidos

--- projeto_laravel/app/Models/Usuario.php
+++ projeto_laravel/app/Models/Usuario.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nome',
+        'sobrenome',
+        'email',
+        'senha',
+        'estatus',
+        'estatus',
+        'imagem',
+        'administrador',
+        'media_de_avaliacoes',
+        'total_de_avaliacoes'
+    ];
 }
