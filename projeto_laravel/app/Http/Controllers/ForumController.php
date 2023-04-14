9a40e92e695492c2f05546400f642dec8a81615d
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-04-14 10:12:19 -0300

    feat: adicionado as funções para edição, criação e exclusão de fóruns

--- projeto_laravel/app/Http/Controllers/ForumController.php
+++ projeto_laravel/app/Http/Controllers/ForumController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 use App\Models\Forum;
 
 class ForumController extends Controller
@@ -21,5 +23,51 @@ class ForumController extends Controller
     public function show($id)
     {
         return view('forums.show', ['forum'=>$this->forum->find($id)]);
+    }
+
+    public function create()
+    {
+        return view('forums.create');
+    }
+
+    public function store(Request $request)
+    {
+        $newForum = $request->all();
+        if (!Forum::create($newForum)) {
+            dd("Error ao criar fórum!!");
+        }
+        return redirect('/forums');
+    }
+
+    public function edit($id)
+    {
+        return view('forums.edit',[
+            'forum'=>Forum::find($id)
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $newForum = $request->all();
+        if (!Forum::find($id)->update($newForum)) {
+            dd("Error ao criar fórum!!");
+        }
+        return redirect('/forums');
+    }
+
+    public function delete($id)
+    {
+        return view('forums.delete',[
+            'forum'=>Forum::find($id)
+        ]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if($request->has('confirmar'))
+            if (!Forum::destroy($id))
+                dd("Error ao deletar fórum!!");
+
+        return redirect('/forums');
     }
 }
