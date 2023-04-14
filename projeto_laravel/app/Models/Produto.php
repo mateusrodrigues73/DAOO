262e9f6316b5a00b440f76c055a9f89829a16c75
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-04-14 09:43:17 -0300

    feat: array fillable adicionado

--- projeto_laravel/app/Models/Produto.php
+++ projeto_laravel/app/Models/Produto.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'modelo',
+        'marca',
+        'categoria',
+        'informacoes',
+        'preco'
+    ];
 }
