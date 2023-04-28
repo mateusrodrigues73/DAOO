d85bdfa60b503c0d835ab996de28f007a59fd4db
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-04-28 10:15:57 -0300

    feat: adicionado a factory de Usuario e Produto na função run

--- projeto_laravel/database/seeders/DatabaseSeeder.php
+++ projeto_laravel/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Usuario::factory(fake()->randomNumber(2))
+            ->hasProdutos(fake()->randomNumber(1))
+            ->create();
     }
 }
