a5b9ce59ac9a3e4f74be1b6227ddce4a88a25c18
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-04 09:17:29 -0300

    feat: alterado o número de linhas a serem gerados no banco de dados de usuarios com produtos

--- projeto_laravel/database/seeders/DatabaseSeeder.php
+++ projeto_laravel/database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Usuario::factory(fake()->randomNumber(2))
-            ->hasProdutos(fake()->randomNumber(1))
+        \App\Models\Usuario::factory(50)
+            ->hasProdutos(5)
             ->create();
     }
 }
