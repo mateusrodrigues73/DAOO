f6420d93d21796dab7f8c51a300d0acfd51cd219
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-05 08:36:14 -0300

    fix: extensão de imagem adicionada de forma correta

--- projeto_laravel/database/factories/ProdutoImagemFactory.php
+++ projeto_laravel/database/factories/ProdutoImagemFactory.php
@@ -17,12 +17,12 @@ class ProdutoImagemFactory extends Factory
     public function definition(): array
     {
         return [
-            'capa' => fake()->word() . 'png',
-            'imagem1' => fake()->word() . 'png',
-            'imagem2' => fake()->word() . 'png',
-            'imagem3' => fake()->word() . 'png',
-            'imagem4' => fake()->word() . 'png',
-            'imagem5' => fake()->word() . 'png'
+            'capa' => fake()->word() . '.png',
+            'imagem1' => fake()->word() . '.png',
+            'imagem2' => fake()->word() . '.png',
+            'imagem3' => fake()->word() . '.png',
+            'imagem4' => fake()->word() . '.png',
+            'imagem5' => fake()->word() . '.png'
         ];
     }
 }
