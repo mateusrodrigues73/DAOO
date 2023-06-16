c8cd2a7d17d5f125d529870dfd551a2b3b28def3
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-16 09:45:53 -0300

    feat: adicionar controller de de produto na api

---
+++ projeto_laravel/app/Http/Controllers/Api/ProdutoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Produto;
+use Illuminate\Http\Request;
+use Exception;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        return response() -> json(Produto::all());
+    }
+
+    public function show($id)
+    {
+        try {
+            return response() -> json(Produto::find($id));
+        } catch (Exception $error) {
+            $responseError = [
+                'Erro' => "O produto de id $id não foi encontrado!",
+                'Exception' => $error->getMessage(),
+            ];
+            $statusHttp = 404;
+            return response() -> json($responseError, $statusHttp);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $newProduto = $request->all();
+            $storedProduto = Produto::create($newProduto);
+            return response()->json([
+                'Message' => "Produto inserido com sucesso",
+                'Produto' => $storedProduto
+            ]);
+        } catch(Exception $error) {
+            $responseError = [
+                'Erro' => "Erro ao inserir novo produto!",
+                'Exception' => $error->getMessage(),
+            ];
+            $statusHttp = 404;
+            return response() -> json($responseError, $statusHttp);
+        }
+    }
+}
