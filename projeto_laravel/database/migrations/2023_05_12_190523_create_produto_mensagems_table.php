8e2a61ba8ff3b58d072bc425323d52cec274eb61
Aluno <aluno@example.com>
2023-06-16 18:03:36 -0300

    fix: remover duplicação da coluna timestamp

--- projeto_laravel/database/migrations/2023_05_12_190523_create_produto_mensagems_table.php
+++ projeto_laravel/database/migrations/2023_05_12_190523_create_produto_mensagems_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
                 ->cascadeOnDelete();
             $table->foreignId('usuario_id')
                 ->references('id')->on('usuarios');
-            $table->timestamps();
         });
     }
 
