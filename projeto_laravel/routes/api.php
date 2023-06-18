2d00a350b08fb2a247e4b975d1c71ebd1e353bca
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-18 11:41:36 -0300

    feat: adicionar rotas para  fóruns

--- projeto_laravel/routes/api.php
+++ projeto_laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ForumController;
 use App\Http\Controllers\Api\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,5 @@ Route::get('/produto/{id}', [ProdutoController::class, 'show']);
 Route::post('/produto', [ProdutoController::class, 'store']);
 Route::put('/produto/{id}', [ProdutoController::class, 'update']);
 Route::delete('/produto/{id}', [ProdutoController::class, 'remove']);
+
+Route::apiResource('forums', ForumController::class);
