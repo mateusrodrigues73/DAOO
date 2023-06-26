b2f3d813ad5ce1610b53bbdcce0467557a467ab2
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-26 09:48:41 -0300

    feat: adicionar controlador de login

---
+++ projeto_laravel/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Exception;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        try{
+            $usuario = Usuario::where('email', $request->email)->first();
+            if(!$usuario || !Hash::check($request->senha, $usuario->senha)) {
+                throw new Exception('Falha na autenticação!');
+            }
+            $ability = $usuario->administrador?['administrador']:[];
+            $token = $usuario->createToken($request->email, $ability)->plainTextToken;
+            return response()->json(['token'=>$token]);
+        }catch(Exception $error){
+            return response()->json([
+                'erro'=>$error->getMessage()
+            ],401);
+        }
+    }
+}
